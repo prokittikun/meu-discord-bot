c052b164c8ab7e478d32f6258bbe7007483a966d
prokittikun <pro.kittikun.2546@gmail.com>
2023-12-29 19:24:35 +0700

    Add: function to merge qrCode into promptpay template

--- src/commands/PromptPayQr.ts
+++ src/commands/PromptPayQr.ts
@@ -9,22 +9,25 @@ import generatePayload from "promptpay-qr";
 import * as path from "path";
 import { generateQRCode } from "../utils/generateQRCode";
 import { config } from "dotenv";
+import { mergeImages } from "../utils/mergeQrImage";
 config();
 export const Qr: Command = {
   name: "qr",
-  description: "Returns a greeting",
+  description: "Returns a PromptPay QR photo",
   type: ApplicationCommandType.ChatInput,
   options: [
     {
       name: "price",
-      description: "The text to include in the greeting",
+      description: "price to pay",
       type: ApplicationCommandOptionType.Number,
       required: true,
     },
   ],
   run: async (client: Client, interaction: CommandInteraction) => {
     const textArgument = interaction.options.get("price")?.value as number;
-    const qrData = generatePayload(process.env.PROMPT_PAY_NUMBER as string, { amount: textArgument });
+    const qrData = generatePayload(process.env.PROMPT_PAY_NUMBER as string, {
+      amount: textArgument,
+    });
     //save to public folder
     const outputFilePath = path.join(
       __dirname,
@@ -34,15 +37,40 @@ export const Qr: Command = {
       "qr",
       "qr.png"
     );
+    const templateImage = path.join(
+      __dirname,
+      "..",
+      "..",
+      "public",
+      "template.png"
+    );
 
     await generateQRCode(qrData, outputFilePath);
+    const outputMergeFilePath = path.join(
+      __dirname,
+      "..",
+      "..",
+      "public",
+      "qr",
+      "output.png"
+    );
+    await mergeImages(
+      templateImage,
+      outputFilePath,
+      outputMergeFilePath,
+      {
+        marginTop: 60,
+        name: "นายรัชชานนท์ รัตนวิจิตร",
+        price: textArgument,
+      }
+    );
 
     const content = `Price is ${textArgument}`;
 
     await interaction.followUp({
       ephemeral: true,
       content,
-      files: [outputFilePath],
+      files: [outputMergeFilePath],
     });
   },
 };
